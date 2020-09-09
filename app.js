d5c18edb7b4baf9c69f14d1d83ac3f46be354e29
msanli220 <mervesanli220@hotmail.com>
2020-09-09 09:45:18 +0300

    changes

--- app.js
+++ app.js
@@ -7,7 +7,37 @@ app.set("view engine", "ejs");
 app.get("/profile", (req, res) => {
   let name = req.query.name;
   let age = req.query.age;
-  res.render("profile.ejs", { name, age }); //  name = name:name
+  res.render("profile.ejs", { name, age }); 
+});
+
+app.get("/news", (req, res) => {
+  let name = req.query.name;
+  let age = req.query.age;
+  res.render("news.ejs", { name, age }); 
+});
+
+app.get("/shop", (req, res) => {
+  let name = req.query.name;
+  let age = req.query.age;
+  res.render("shop.ejs", { name, age }); 
+});
+
+app.get("/songs", (req, res) => {
+  let name = req.query.name;
+  let age = req.query.age;
+  res.render("songs.ejs", { name, age }); 
+});
+
+app.get("/events", (req, res) => {
+  let name = req.query.name;
+  let age = req.query.age;
+  res.render("events.ejs", { name, age }); 
+});
+
+app.get("/videos", (req, res) => {
+  let name = req.query.name;
+  let age = req.query.age;
+  res.render("videos.ejs", { name, age }); 
 });
 
 app.listen(80, () => {
